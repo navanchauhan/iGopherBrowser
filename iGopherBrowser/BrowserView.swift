dd94f794224e9527ef410956934e9fc70afe53d9
Navan Chauhan <navanchauhan@gmail.com>
2023-12-16 18:30:40 -0700

    v0.9.1

---
+++ iGopherBrowser/BrowserView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowserView.swift
+//  iGopherBrowser
+//
+//  Created by Navan Chauhan on 12/16/23.
+//
+
+import SwiftUI
+
+struct BrowserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowserView()
+}
